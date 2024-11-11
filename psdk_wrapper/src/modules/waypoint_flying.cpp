83681fbe3d6f42715b04390e2f91fc392b50cd5f
Tommy Persson <tommy.persson@liu.se>
2024-11-11 12:30:15 +0100

    Change for psdk.

--- psdk_wrapper/src/modules/waypoint_flying.cpp
+++ psdk_wrapper/src/modules/waypoint_flying.cpp
@@ -307,8 +307,8 @@ T_DjiReturnCode WaypointFlyingModule::state_callback(T_DjiWaypointV2MissionState
 }
     
 T_DjiReturnCode state_callback2(T_DjiWaypointV2MissionStatePush stateData) {
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "state_callback2: %d %d %d",
-              stateData.curWaypointIndex, stateData.state, stateData.velocity);
+  //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "state_callback2: %d %d %d",
+  //              stateData.curWaypointIndex, stateData.state, stateData.velocity);
   return 0;
 }
     
