e56597da9d17eeb7ef5c4cefae531f87810e425c
Piotr Rudol <piotr.rudol@liu.se>
2026-06-29 12:20:40 +0200

    adding widget module

--- psdk_wrapper/src/modules/liveview.cpp
+++ psdk_wrapper/src/modules/liveview.cpp
@@ -84,7 +84,7 @@ LiveviewModule::get_camera_info(E_DjiCameraType camera_type,
           file_path+="H20T_wide.yml";
           RCLCPP_INFO(get_logger(), "Loading camera calibration file: %s", file_path.c_str());
           camera_info_manager_->loadCameraInfo(file_path);
-          camera_infos_[DJI_LIVEVIEW_CAMERA_SOURCE_H20T_ZOOM] =
+          camera_infos_[DJI_LIVEVIEW_CAMERA_SOURCE_H20T_WIDE] =
               camera_info_manager_->getCameraInfo();
           break;
 
