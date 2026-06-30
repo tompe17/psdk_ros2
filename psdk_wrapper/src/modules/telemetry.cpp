1d5ff395e13fcc4c228f1ab7ec0743eb07e8f168
Piotr Rudol <piotr.rudol@liu.se>
2026-06-30 14:32:25 +0200

    fixing timing

--- psdk_wrapper/src/modules/telemetry.cpp
+++ psdk_wrapper/src/modules/telemetry.cpp
@@ -44,7 +44,7 @@ TelemetryModule::~TelemetryModule()
 
 
 rclcpp::Time TelemetryModule::get_measurement_time(
-    const T_DjiDataTimestamp &timestamp)
+    const T_DjiDataTimestamp *timestamp)
 {
   auto ros_now = this->get_clock()->now();
 
