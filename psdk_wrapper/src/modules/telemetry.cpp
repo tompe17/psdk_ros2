cf5ee0b3b608c30042a24aec6fe9830829143ddc
Piotr Rudol <piotr.rudol@liu.se>
2026-07-21 07:55:57 +0200

    working on tf

--- psdk_wrapper/src/modules/telemetry.cpp
+++ psdk_wrapper/src/modules/telemetry.cpp
@@ -2957,7 +2957,7 @@ TelemetryModule::get_yaw_gimbal()
 
   /* Get current gimbal yaw wrt to East */
   double current_gimbal_yaw = current_state_.gimbal_angles.vector.z;
-  return current_gimbal_yaw - current_yaw;
+  return current_gimbal_yaw + current_yaw;
 }
 
 std::string
