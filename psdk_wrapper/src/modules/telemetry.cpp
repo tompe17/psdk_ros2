b8ec01d644aa19fddcb13c54f2a9f3f6a41e8994
Piotr Rudol <piotr.rudol@liu.se>
2026-07-21 08:30:36 +0200

    working on tf

--- psdk_wrapper/src/modules/telemetry.cpp
+++ psdk_wrapper/src/modules/telemetry.cpp
@@ -1460,7 +1460,7 @@ TelemetryModule::gimbal_angles_callback(const uint8_t *data, uint16_t data_size,
   gimbal_angles_msg.vector.y = psdk_utils::deg_to_rad(-gimbal_angles->x);
   gimbal_angles_msg.vector.z =
       psdk_utils::SHIFT_N2E -
-      psdk_utils::deg_to_rad(gimbal_angles->z + body_gimbal_offset_deg_);
+      psdk_utils::deg_to_rad(gimbal_angles->z - body_gimbal_offset_deg_);
   // gimbal_angles_msg.vector.z =
   //     psdk_utils::SHIFT_N2E -
   //     psdk_utils::deg_to_rad(gimbal_angles->z);
