983d0b555826d3e419dfe23de0aac8d6b215fc50
Piotr Rudol <piotr.rudol@liu.se>
2026-07-21 14:35:52 +0200

    working on gimbal stuff

--- psdk_wrapper/src/modules/telemetry.cpp
+++ psdk_wrapper/src/modules/telemetry.cpp
@@ -1481,12 +1481,12 @@ TelemetryModule::gimbal_angles_callback(const uint8_t *data, uint16_t data_size,
     // only in follow mode?
     if (global_gimbal_ptr_->gimbal_mode_ == DJI_GIMBAL_MODE_YAW_FOLLOW)
     {
-      // gimbal_angles_msg.vector.z +=
-      // body_yaw_raw_at_reset_rad_ - get_body_yaw_raw_rad(false);
+      gimbal_angles_msg.vector.z +=
+      body_yaw_raw_at_reset_rad_ - get_body_yaw_raw_rad(false);
 
-      auto diff = get_body_yaw_raw_rad(false) - get_body_yaw_raw_rad(true);
-      RCLCPP_INFO(get_logger(), "Angle diff: %f ", psdk_utils::rad_to_deg(diff));
-      gimbal_angles_msg.vector.z += diff*10;
+      // auto diff = get_body_yaw_raw_rad(false) - get_body_yaw_raw_rad(true);
+      // RCLCPP_INFO(get_logger(), "Angle diff: %f ", psdk_utils::rad_to_deg(diff));
+      // gimbal_angles_msg.vector.z += diff;
 
       // body_yaw_raw_at_reset_rad_ - get_body_yaw_raw_rad(false);
     }
