7c9748a715812b907bc25234e2f39848d07543d2
Piotr Rudol <piotr.rudol@liu.se>
2026-07-21 10:19:18 +0200

    working on tf

--- psdk_wrapper/launch/wrapper.launch.py
+++ psdk_wrapper/launch/wrapper.launch.py
@@ -147,6 +147,7 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # ld.add_action(LogInfo(msg=["tf_frame_prefix=", tf_frame_prefix_arg]))
+    ld.add_action(LogInfo(msg=["location=", declare_location_arg]))
 
     # Declare Launch options
     ld.add_action(declare_namespace_cmd)
