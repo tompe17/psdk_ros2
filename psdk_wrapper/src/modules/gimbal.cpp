e92fe398283c89eccc3ddabb9d0748940e4c53d3
Piotr Rudol <piotr.rudol@liu.se>
2026-07-29 13:55:28 +0200

    gimbal angles

--- psdk_wrapper/src/modules/gimbal.cpp
+++ psdk_wrapper/src/modules/gimbal.cpp
@@ -279,7 +279,7 @@ GimbalModule::rotate_gimbal(E_DjiMountPosition index,
 
     auto t = global_telemetry_ptr_;
     if (t->params_.sim)
-    {}
+    {
     // if (global_gimbal_ptr_->gimbal_mode_ == DJI_GIMBAL_MODE_YAW_FOLLOW)
     // {
     auto offset_due_to_yaw = t->body_yaw_raw_at_reset_rad_ - t->get_body_yaw_raw_rad();
@@ -287,7 +287,7 @@ GimbalModule::rotate_gimbal(E_DjiMountPosition index,
     rotation_deg.yaw += psdk_utils::rad_to_deg(offset_due_to_yaw);
     // pioru: test if needed
      // rotation_deg.yaw += global_telemetry_ptr_->body_gimbal_offset_raw_deg_;
-  }
+    }
   }
 
   rotation_deg.time = time;
